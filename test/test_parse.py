6f1e7bd985152f0b20d02962ce7b991b75568ad4
Jobanpreet Singh <94118977+jobanpreet495@users.noreply.github.com>
2024-11-20 10:24:08 +0530

    Update test_parse.py

--- test/test_parse.py
+++ test/test_parse.py
@@ -1,7 +1,7 @@
 from parsepro.parsepro import ImageToMarkdown
 
 # Initialize the converter with an API key
-converter = ImageToMarkdown(api_key="deb0b57b8ea6f67a5caf84b5944d3bdfe9fd6d6b603d9bbeccd5dbc936f39d2f")
+converter = ImageToMarkdown(api_key="your togetherai api key")
 
 # Convert the image and get Markdown content
 markdown_content = converter.convert_image_to_markdown("/home/erginous/Desktop/Invoice/a.png")
