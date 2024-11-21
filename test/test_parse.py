a6017ad65b6a059f6597ff1d4b5fd3bab60f28b3
Jobanpreet Singh <94118977+jobanpreet495@users.noreply.github.com>
2024-11-21 13:21:03 +0530

    Delete test directory

--- test/test_parse.py
+++
@@ -1,8 +0,0 @@
-from parsepro.parsepro import ImageToMarkdown
-
-# Initialize the converter with an API key
-converter = ImageToMarkdown(api_key="your togetherai api key")
-
-# Convert the image and get Markdown content
-markdown_content = converter.convert_image_to_markdown("/home/erginous/Desktop/Invoice/a.png")
-print(markdown_content)
